f8740d0ccf798663f34c27c0b9307c653b8147e3
ArinaBabat <106030709+ArinaBabat@users.noreply.github.com>
2023-12-01 10:00:33 +0300

    Update ano.sh

--- train_files/ano.sh
+++ train_files/ano.sh
@@ -1,9 +1,9 @@
-for i in $( seq 1 50)
+for i in $( seq 1 50)    # Цикл, i от 1 до 50
 do
-    python generate_data.py anonymous --file_count ${i} --njobs 5 --train_size 10000 --valid_size 4000
-    if [ $((i%10)) -eq 0 ];then
-    python train.py anonymous  --exp_name 3_anonymous_dagger --file_count ${i} --epoch 100
+    python generate_data.py anonymous --file_count ${i} --njobs 5 --train_size 10000 --valid_size 4000    # Запуск скрипта generate_data.py с параметрами. Здесь ${i} - это текущее значение переменной i. Вероятно, создает искусственные данные для обучения модели
+    if [ $((i%10)) -eq 0 ];then    # Если i делится на 10 без остатка
+    python train.py anonymous  --exp_name 3_anonymous_dagger --file_count ${i} --epoch 100    # 100 эпох обучения
     else
-    python train.py anonymous  --exp_name 3_anonymous_dagger --file_count ${i} --epoch 10
+    python train.py anonymous  --exp_name 3_anonymous_dagger --file_count ${i} --epoch 10    # 10 эпох
     fi
 done
